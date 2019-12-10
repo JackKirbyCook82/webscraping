c5abe9b7fcd2ec5e35db14315ecb021de2281d37
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-12-09 21:03:11 -0800

    Updated

---
+++ url.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 2018
+@name:   URL Objects
+@author: Jack Kirby cook
+"""
+
+from collections import namedtuple as ntuple
+
+from utilities.sgmts import value_segment, argument_segment, keyword_segment
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = ['URL', 'Protocol', 'Domain', 'Port', 'Path', 'Parms', 'Anchor', 'JSONPath', 'HTMLPath', 'CSVPath', 'ZIPPath', 'EXEPath']
+__copyright__ = "Copyright 2018, Jack Kirby Cook"
+__license__ = ""
+
+
+Protocol = value_segment('Protocol', endchar='://')
+Domain = value_segment('Domain')
+Port = value_segment('Port', startchar=':')
+Path = argument_segment('Path', '/', startchar='/', endchar='') 
+
+JSONPath = argument_segment('JSONPath', '/', startchar='/', endchar='.json') 
+HTMLPath = argument_segment('HTMLPath', '/', startchar='/', endchar='.html')  
+CSVPath = argument_segment('CSVPath', '/', startchar='/', endchar='.csv') 
+ZIPPath = argument_segment('FTPPath', '/', startchar='/', endchar='.zip')
+EXEPath = argument_segment('EXEPath', '/', startchar='/', endchar='.exe')
+
+Parms = keyword_segment('Parms', '&', '=', startchar='?') 
+Anchor = value_segment('Port', startchar='#')
+
+                                       
+url_segments = ('protocol', 'domain', 'port', 'path', 'parms', 'anchor')                       
+URL_Segments = ntuple('UrlSgmts', ' '.join(url_segments))          
+
+
+class URL(URL_Segments):   
+    def __new__(cls, protocol, domain, port=Port(), path=Path(), parms=Parms(), anchor=Anchor()):
+        return super().__new__(cls, protocol, domain, port, path, parms, anchor)
+    def __str__(self): 
+        return ''.join([str(value) for value in self._asdict().values() if value])
+
+
+
+    
---
+++ webreaders.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 2019
+@name:   Webreader Objects
+@author: Jack Kirby Cook
+
+"""
+
+import requests
+from requests.adapters import HTTPAdapter
+from requests.auth import HTTPBasicAuth
+from requests.packages.urllib3.util.retry import Retry
+import time
+from functools import update_wrapper
+
+from utilities.dispatchers import clskey_singledispatcher as keydispatcher
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = ['WebReader']
+__copyright__ = "Copyright 2018, Jack Kirby Cook"
+__license__ = ""
+
+
+_RETRY = {'retries':3, 'backoff':0.3, 'httpcodes':(500, 502, 504)}
+_DELAY = 0
+_LASTTIME = None
+
+
+def _remember(stoptime):
+    global _LASTTIME
+    _LASTTIME = stoptime
+
+
+def sleeper(delay):
+    ready = lambda current, last: current - last  > delay if last else True
+    wait = lambda current, last: max([delay - int(current - last), 0]) if last else 0
+    
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            starttime = time.time()
+            if not ready(starttime, _LASTTIME): 
+                waittime = wait(starttime, _LASTTIME)
+                print('Download Waiting: {} Seconds'.format(waittime))
+                time.sleep(waittime)
+            response = function(*args, **kwargs)
+            stoptime = time.time()
+            _remember(stoptime)
+            return response
+        
+        update_wrapper(wrapper, function)
+        return wrapper
+    return decorator
+
+
+def create_retry(retries=3, backoff=0.3, httpcodes=[500, 502, 504], **kwargs):
+    retry = Retry(total=retries, read=retries, connect=retries, backoff_factor=backoff, status_forcelist=httpcodes)
+    adapter = HTTPAdapter(max_retries=retry)
+    return adapter
+
+
+class WebReader(object): 
+    requestmethods = {'get', 'post'}    
+    datamethods = {'html': lambda response: response.text, 
+                   'json': lambda response: response.json(), 
+                   'zip' : lambda response: response.content,
+                   'csv' : lambda response: response.content.deconde('utf-8')}
+   
+    def __new__(cls, *args, delay=_DELAY, **kwargs):
+        instance = super().__new__(cls)
+        instance.get = sleeper(delay)(instance.execute) if delay else instance.get
+        return instance
+        
+    def __init__(self, *args, headers={}, retry=_RETRY, **kwargs):
+        self.__headers = headers     
+        self.__retry = retry                
+    
+    @keydispatcher
+    def execute(self, method, session, url, *args, **kwargs): raise KeyError(method)
+    @execute.register('get')
+    def __get(self, session, url, *args, **kwargs): return session.get(url)
+    @execute.register('post')
+    def __post(self, session, url, *args, data={}, files={}, **kwargs): return session.post(url, data=data, files=files)
+
+    def __call__(self, url, *args, method, datatype, cookies={}, username=None, password=None, **kwargs):
+        assert method in self.requestmethods
+        assert datatype in self.datamethods
+        with requests.Session() as session:
+            if self.__headers: session.headers.update(self.__headers)
+            if cookies: session.cookies.update(cookies)              
+            if username: session.auth = HTTPBasicAuth(username, password)
+            retryadapter = create_retry(**self.__retry)
+            session.mount('http://', retryadapter)
+            session.mount('https://', retryadapter)
+            
+            response = self.execute(method, session, url, *args, **kwargs)
+            if not response.status_code == requests.codes.ok: print('URL Request Failure:')
+            else: print('URL Request Success:')  
+            print(str(url), '\n')
+            response.raise_for_status()
+            data = self.datamethods[datatype](response)
+        return data
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
