24a7bcd9c04a1b0b4608ea18c18905ca7809ce04
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-01-02 18:00:26 -0800

    Updated'

---
+++ webdriver.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 30 2019
+@name:   Webdriver Objects
+@author: Jack Kirby Cook
+
+"""
+
+__version__ = "1.0.0"
+__author__ = "Jack Kirby Cook"
+__all__ = []
+__copyright__ = "Copyright 2018, Jack Kirby Cook"
+__license__ = ""
