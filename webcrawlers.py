e4209e4081093360d1f6baea121e90bbb1e42240
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-01-15 11:45:35 -0800

    Updated

--- webcrawlers.py
+++ webcrawlers.py
@@ -15,6 +15,9 @@ __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
 
+_aslist = lambda items: [items] if not isinstance(items, (list, tuple)) else list(items)
+
+
 class WebCrawler(object):
     keyformat = lambda mainnum, pagenum: "{}|{}".format(mainnum, pagenum)
     treeparsers = {'html': etree.HTMLParser()}
@@ -37,13 +40,13 @@ class WebCrawler(object):
 
     def execute(self, mainurl, *args, crawlxpath, pagexpath, crawlnum=0, pagenum=0, datatype='html', **kwargs):
         mainhtmlpage = self.__webreader(mainurl, *args, method='get', datatype=datatype, **kwargs)
-        mainhtmltree = etree.parse(mainhtmlpage, self.treeparsers[self.__treeparser])
+        mainhtmltree = etree.parse(mainhtmlpage, self.treeparsers[self.__treeparser]) ######
         
-        for pageurl in mainhtmltree.xpath(pagexpath):    
+        for pageurl in _aslist(mainhtmltree.xpath(pagexpath)): ######  
             pagenum = pagenum + 1
             yield crawlnum, pagenum, pageurl        
         
-        nexturl = mainhtmltree.xpath(crawlxpath)[0] 
+        nexturl = mainhtmltree.xpath(crawlxpath) ######
         yield from self.execute(nexturl, *args, crawlnum=crawlnum+1, pagenum=pagenum, **kwargs)
         
 
