633b8c218173935f7372be17b90684a5a12894eb
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-01-30 13:51:53 -0800

    Updated

--- webdrivers.py
+++ webdrivers.py
@@ -8,6 +8,7 @@ Created on Mon Dec 30 2019
 
 from abc import ABC, abstractmethod
 import time
+import random
 from selenium import webdriver
 
 __version__ = "1.0.0"
@@ -21,14 +22,14 @@ class WebDriver(ABC):
     webdrivers = {'chrome':webdriver.Chrome, 'firefox':webdriver.Firefox}
     weboptions = {'chrome':webdriver.ChromeOptions, 'firefox':webdriver.FirefoxOptions}
 
-    def __init__(self, file, browser, *args, delay=None, patience=None, **kwargs): 
+    def __init__(self, file, browser, *args, mindelay=5, maxdelay=20, patience=100, **kwargs): 
         self.__file, self.__browser =  file, browser
         self.__patience = patience
-        self.__sleep = delay
+        self.__mindelay, self.__maxdelay = mindelay, maxdelay
     
     def __call__(self, url, *args, **kwargs): 
         self.__driver = self.webdrivers[self.__browser.lower()](executable_path=self.__file, options=self.weboptions[self.__browser]()) 
-        if self.__patience: self.__driver.set_page_load_timeout(self.__patience)
+        self.__driver.set_page_load_timeout(self.__patience)
         self.__driver.maximize_window()
         self.__driver.get(str(url))
         yield from self.execute(*args, **kwargs)
@@ -46,7 +47,7 @@ class WebDriver(ABC):
     
     def back(self): self.__driver.back
     def forward(self): self.__driver.forward
-    def sleep(self): time.sleep(self.__sleep)
+    def sleep(self): time.sleep(random.randint(self.__mindelay, self.__maxdelay))
     def refresh(self): self.__driver.refresh
 
 
--- webelements.py
+++ webelements.py
@@ -7,7 +7,10 @@ Created on Mon Dec 30 2019
 """
 
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
+from utilities.dataframes import dataframe_fromhtml
+ 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
 __all__ = ['WebElement', 'WebButton', 'WebLink', 'WebLinks', 'WebSelect', 'WebInput', 'WebData', 'WebTable']
@@ -18,13 +21,18 @@ __license__ = ""
 class WebElement(object): 
     @property
     def element(self): return self.__element
+    @property
+    def html(self): return self.__element.get_attribute('outerHTML')    
+    
     def __init__(self, *args, **kwargs): 
         try: self.__element = self.fromelement(*args, **kwargs)
         except TypeError: self.__element = self.fromdriver(*args, **kwargs)
     
-    def fromdriver(self, *args, driver, **kwargs): return driver.find_element(By.XPATH, self.xpath)
     def fromelement(self, *args, element, **kwargs): return element
-
+    def fromdriver(self, *args, driver, **kwargs): 
+        try: return driver.find_element(By.XPATH, self.xpath)
+        except NoSuchElementException: return None
+    
     @classmethod
     def create(cls, xpath, **kwargs):
         def wrapper(subclass): return type(subclass.__name__, (subclass, cls), dict(xpath=xpath, **kwargs))
@@ -43,6 +51,7 @@ class WebLinks(WebElement):
     def fromdriver(self, *args, driver, **kwargs): 
         try: return {key:value for key, value in zip(driver.find_elements(By.XPATH, self.keys), driver.find_elements(By.XPATH, self.xpath))}
         except AttributeError: return {index:value for index, value in enumerate(driver.find_elements(By.XPATH, self.xpath), start=0)}
+    
     def click(self, key): self.element[key].click()   
     def __getitem__(self, key): return WebLink(element=self.element[key])
     def __iter__(self): 
@@ -52,6 +61,7 @@ class WebLinks(WebElement):
 class WebSelect(WebElement):
     def fromdriver(self, *args, driver, **kwargs): 
         return {key:value for key, value in zip(driver.find_elements(By.XPATH, self.keys), driver.find_elements(By.XPATH, self.xpath))}
+    
     def select(self, key): self.element[key].click()   
     def selections(self): return {key:element.is_selected() for key, element in self.element.items()}
     
@@ -59,14 +69,31 @@ class WebSelect(WebElement):
 class WebInput(WebElement):
     def click(self): self.element.click()
     def clear(self): self.element.clear()
+    def fill(self, content): self.element.sendKeys(content)
 
 
 class WebData(WebElement):
-    def __call__(self): pass
+    delimiter = '|'
+    def fromdriver(self, *args, driver, **kwargs): return [item for item in driver.find_elements(By.XPATH, self.xpath)]    
+    
+    def __str__(self): return self.delimiter.join([element.text for element in self.element])
+    def __getitem__(self, index): return self.element[index].text
+    def __iter__(self): 
+        for element in self.element: yield element.text
 
 
 class WebTable(WebElement):
-    def __call__(self): pass
+    def asdict(self):
+        dataframe = dataframe_fromhtml(self.html, tablenum=0, header=0, htmlparser='lxml', forceframe=True)
+        return dataframe.to_dict(orient='list')
+
+
+
+
+
+
+
+
 
 
 
