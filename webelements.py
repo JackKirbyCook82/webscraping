0362ea50a947bd12fbc61dac2e8e329f6f12e9cd
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-10-08 17:56:41 -0700

    Updated

--- webelements.py
+++ webelements.py
@@ -11,67 +11,121 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 
-from webscraping.elements import Element, Clickable, Link, Text, Table, Input, Selection
+from webscraping.elements import Clickable, Link, Text, Table, Input, Selection
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ['WebClickable', 'WebButton', 'WebRadioButton', 'WebCheckBox', 'WebText', 'WebTable', 'WebInput', 'WebSelection', 'WebLink', 'WebTexts', 'WebLinks']
+__all__ = ['WebClickable', 'WebButton', 'WebRadioButton', 'WebCheckBox', 'WebText', 'WebTable', 'WebInput', 'WebSelection', 'WebLink', 'WebClickables', 'WebLinks']
 __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
+
+WEBELEMENT_REGISTRY = {}
+WEBELEMENTS_REGISTRY = {}
       
 _aslist = lambda items: [items] if not isinstance(items, (tuple, list)) else list(items)
 
 
+def getelement(driver, timeout, xpath):
+    try: element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, xpath)))
+    except (NoSuchElementException, TimeoutException, WebDriverException): element = None        
+    return element    
+
+def getelements(driver, timeout, xpath): 
+    try: elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
+    except (NoSuchElementException, TimeoutException, WebDriverException): elements = []  
+    return elements
+    
+
 class WebElement(object):
-    __registry = []
-    def __init_subclass__(cls, *args, xpath=None, element=None, **attrs): 
-        if cls in WebElement.__subclasses__():
-            assert element is not None and issubclass(element, Element)           
-            setattr(cls, 'Element', element)                 
-        else: 
-           assert xpath is not None        
-           cls.Element.update(**attrs)
-           setattr(cls, 'xpath', xpath)  
-           WebElement.__registry.append(cls)    
-            
+    @classmethod
+    def addchild(cls, key, child):
+        assert key is not None
+        cls.Children[key] = child
+        
+    def __init_subclass__(cls, *args, xpath=None, element=None, parent=None, key=None, **attrs):
+        if element is not None: setattr(cls, 'Element', element)
+        if xpath is not None: setattr(cls, 'xpath', xpath)
+        cls.Element.update(**attrs)
+        if hasattr(cls, 'xpath') and hasattr(cls, 'Element'): 
+            setattr(cls, 'Children', {})
+            if parent is None: WEBELEMENT_REGISTRY[cls.__name__] = cls
+            else: WEBELEMENT_REGISTRY[parent.__name__].addchild(key, cls)
+           
     __instance = None
-    def __init__(self, driver, timeout, *args, **kwargs): self.element = self.Element(self.get(driver, timeout))    
-    def __str__(self): return "{}|{}".format(self.__class__.__name__, str(bool(self.element)))
-    def __getattr__(self, attr): return getattr(self.element, attr)
-    def __new__(cls, *args, **kwargs):
-        assert cls in WebElement.__registry and hasattr(cls, 'xpath') and hasattr(cls, 'Element')
-        if cls.__instance is None: cls.__instance = super().__new__(cls) 
-        return cls.__instance  
-     
-    def get(self, driver, timeout):        
-          print("WebElement Loading: {}".format(self.__class__.__name__))
-          try: element = WebDriverWait(driver, timeout).until(EC.presence_of_element_located((By.XPATH, self.xpath)))
-          except (NoSuchElementException, TimeoutException, WebDriverException): element = None        
-          if element is None: print("WebElement Missing: {}".format(self.__class__.__name__))  
-          return element
-
-
-class WebElements(list):
-    def __init_subclass__(cls, *args, xpath=None, element=None, **attrs): 
-        if cls in WebElements.__subclasses__():
-            assert element is not None and issubclass(element, Element)
-            setattr(cls, 'Element', element)
-        else: 
-            assert xpath is not None
-            cls.Element.update(**attrs)
-            setattr(cls, 'xpath', xpath)            
-
-    def __init__(self, driver, timeout, *args, **kwargs): super().__init__([self.Element(element) for element in self.get(driver, timeout)])
-    def __str__(self): return "{}|({})".format(self.__class__.__name__, ', '.join([str(bool(element)) for element in self]))
     def __new__(cls, *args, **kwargs):
         assert hasattr(cls, 'xpath') and hasattr(cls, 'Element')
+        assert cls.__name__ in WEBELEMENT_REGISTRY.keys() and cls in WEBELEMENT_REGISTRY.values()
+        if cls.__instance is None: cls.__instance = super().__new__(cls) 
+        return cls.__instance  
+    
+    def __init__(self, driver, timeout, *args, **kwargs):
+        if getattr(self, 'initialized', False): return
+        self.__element = self.Element(self.get(driver, timeout))
+        self.__children = {key:child(self.element, timeout, *args, **kwargs) for key, child in self.Children.items()}
+        self.initialized = True
+
+    @property
+    def element(self): return self.__element
+    @property
+    def children(self): return self.__children
+    
+    def __bool__(self): return bool(self.__element)
+    def __str__(self): return "{}|{}".format(self.__class__.__name__, str(bool(self.__element)))      
+    def __getattr__(self, attr): return getattr(self.element, attr)   
+    def __getitem__(self, key): return self.__children[key] 
+       
+    def get(self, driver, timeout):
+        print("WebElement Loading: {}".format(self.__class__.__name__))    
+        element = getelement(driver, timeout, self.xpath)
+        if element is None: print("WebElement Missing: {}".format(self.__class__.__name__))         
+        return element
+    
+
+class WebElements(object):    
+    @classmethod
+    def addchild(cls, key, child):
+        assert key is not None
+        cls.Children[key] = child
+    
+    def __init_subclass__(cls, *args, xpath=None, element=None, parent=None, key=None, **attrs):
+        if element is not None: setattr(cls, 'Element', element)
+        if xpath is not None: setattr(cls, 'xpath', xpath)
+        cls.Element.update(**attrs)
+        if hasattr(cls, 'xpath') and hasattr(cls, 'Element'): 
+            setattr(cls, 'Children', {})
+            if parent is None: WEBELEMENTS_REGISTRY[cls.__name__] = cls
+            else: WEBELEMENTS_REGISTRY[parent.__name__].addchild(key, cls)
+
+    def __new__(cls, *args, **kwargs):
+        assert hasattr(cls, 'xpath') and hasattr(cls, 'Element') 
+        assert cls.__name__ in WEBELEMENTS_REGISTRY.keys() and cls in WEBELEMENTS_REGISTRY.values()
         return super().__new__(cls)   
+
+    def __init__(self, driver, timeout, *args, **kwargs):
+        self.__elements = [self.Element(element) for element in self.get(driver, timeout)]
+        self.__childrens = [{} for element in self.__elements]
+        for index, webelement in enumerate(self.__elements):
+            self.__children[index]
+            self.__children[index].update({key:child(webelement, timeout, *args, **kwargs) for key, child in self.Children.items()})
+
+    @property
+    def elements(self): return self.__elements
+    @property
+    def childrens(self): return self.__children
+
+    def __bool__(self): return all([bool(element) for element in self.__elements])
+    def __str__(self): return "{}|({})".format(self.__class__.__name__, ', '.join([str(bool(element)) for element in self.__elements]))  
+    def __getitem__(self, locator): 
+        if isinstance(locator, int): return self.__elements[locator]
+        elif isinstance(locator, tuple): 
+            assert len(locator) == 2 and isinstance(locator[0], int) and isinstance(locator[1], str)
+            return self.__children[locator[0]][locator[1]]
+        else: raise TypeError(type(locator).__name__)
         
     def get(self, driver, timeout):
         print("WebElements Loading: {}".format(self.__class__.__name__))
-        try: elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located((By.XPATH, self.xpath)))
-        except (NoSuchElementException, TimeoutException, WebDriverException): elements = []  
+        elements = getelements(driver, timeout, self.xpath)
         if not elements: print("WebElements Missing: {}".format(self.__class__.__name__))  
         return elements
 
@@ -85,8 +139,10 @@ class WebTable(WebElement, element=Table): pass
 class WebInput(WebElement, element=Input): pass
 class WebSelection(WebElement, element=Selection): pass
 class WebLink(WebElement, element=Link): pass
-class WebTexts(WebElements, element=Text): pass
+
+class WebClickables(WebElements, element=Clickable): pass
 class WebLinks(WebElements, element=Link): pass
+
 
 
 
