187a70bbadcc5d38df44b7b8e70da19136241045
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-05-24 23:27:59 -0500

    Updated 1/10/2024

--- webpayloads.py
+++ webpayloads.py
@@ -21,6 +21,7 @@ class WebPayloadError(Exception): pass
 class WebPayloadMissingError(WebPayloadError): pass
 class WebPayloadSingleError(WebPayloadError): pass
 class WebPayloadMultipleError(WebPayloadError): pass
+class WebPayloadTypingError(WebPayloadError): pass
 
 
 class WebPayloadMeta(AttributeMeta, TreeMeta, ABCMeta):
@@ -28,6 +29,9 @@ class WebPayloadMeta(AttributeMeta, TreeMeta, ABCMeta):
         super(WebPayloadMeta, cls).__init__(*args, **kwargs)
         cls.__optional__ = kwargs.get("optional", getattr(cls, "__optional__", False))
         cls.__locator__ = kwargs.get("locator", getattr(cls, "__locator__", None))
+
+    def extract(cls, source):
+        pass
 
     @property
     def optional(cls): return cls.__optional__
@@ -41,9 +45,9 @@ class WebCollectionMeta(WebPayloadMeta):
         cls.__multiple__ = kwargs.get("multiple", getattr(cls, "__multiple__", False))
 
     def __call__(cls, sources, *args, **kwargs):
-        if cls.multiple and not isinstance(sources, list): raise WebPayloadMultipleError()
-        elif not cls.multiple and isinstance(sources, list): raise WebPayloadSingleError()
-        if not cls.optional and not bool(sources): raise WebPayloadMissingError()
+        if cls.multiple and not isinstance(sources, list): raise WebPayloadSingleError()
+        elif not cls.multiple and isinstance(sources, list): raise WebPayloadMultipleError()
+        if cls.multiple and not cls.optional and not bool(sources): raise WebPayloadMissingError()
         initialize = lambda source: super(WebCollectionMeta, cls).__call__(source, *args, **kwargs)
         if cls.multiple: return [initialize(source) for source in sources]
         else: return initialize(sources)
@@ -55,26 +59,39 @@ class WebCollectionMeta(WebPayloadMeta):
 class WebMappingMeta(WebPayloadMeta):
     def __init__(cls, *args, **kwargs):
         super(WebMappingMeta, cls).__init__(*args, **kwargs)
-        cls.__mapping__ = getattr(cls, "__mapping__", {}) | kwargs.get("mapping", {})
+        cls.__static__ = getattr(cls, "__static__", {}) | kwargs.get("static", {})
 
     def __call__(cls, source, *args, **kwargs):
-        children = {dependent.locator: dependent(source, *args, **kwargs) for dependent in cls.dependents}
-        source = children | cls.mapping
-        instance = super(WebMappingMeta, cls).__call__(source)
-        return instance
+#        source = source[cls.key] if cls.key is not None else source
+        if not isinstance(source, dict): raise WebPayloadTypingError()
+        if not cls.optional and not bool(source): raise WebPayloadMissingError()
+        children = {dependent.locator: dependent(source, *args, **kwargs) for dependent in cls.dependents.values()}
+        return cls.static | children
 
     @property
-    def mapping(cls): return cls.__mapping__
+    def static(cls): return cls.__static__
+
+
+class WebTextMeta(WebPayloadMeta):
+    def __init__(cls, *args, **kwargs):
+        super(WebTextMeta, cls).__init__(*args, **kwargs)
+        cls.__parser__ = kwargs.get("parser", getattr(cls, "__parser__", str))
+
+    def __call__(cls, source, *args, **kwargs):
+#        source = source[cls.key] if cls.key is not None else source
+        if not isinstance(source, (str, int, float)): raise WebPayloadTypingError()
+        if not cls.optional and source is None: raise WebPayloadMissingError()
+        return cls.parser(source)
+
+    @property
+    def parser(cls): return cls.__parser__
 
 
 class WebPayloadMappingMeta(WebCollectionMeta, WebMappingMeta): pass
-class WebPayloadTextMeta(WebCollectionMeta): pass
+class WebPayloadTextMeta(WebCollectionMeta, WebTextMeta): pass
 
 
-class WebPayload(ABC, metaclass=WebPayloadMeta):
-    pass
-
-
+class WebPayload(ABC, metaclass=WebPayloadMeta): pass
 class WebPayloadMapping(WebPayload, metaclass=WebPayloadMappingMeta, attribute="Mapping"): pass
 class WebPayloadText(WebPayload, metaclass=WebPayloadTextMeta, attribute="Text"): pass
 
