bcc0018e4a723a2800cd66661fb57a940b7a6ad3
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-05-24 22:51:00 -0500

    Updated 1/10/2024

--- webpayloads.py
+++ webpayloads.py
@@ -7,7 +7,6 @@ Created on Tues May 19 2026
 """
 
 from abc import ABC, ABCMeta
-from collections import OrderedDict as ODict
 
 from support.meta import AttributeMeta, TreeMeta
 
@@ -17,61 +16,69 @@ __all__ = ["WebPayload", "WebPayloadError"]
 __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = "MIT License"
 
+
 class WebPayloadError(Exception): pass
 class WebPayloadMissingError(WebPayloadError): pass
 class WebPayloadSingleError(WebPayloadError): pass
 class WebPayloadMultipleError(WebPayloadError): pass
-class WebPayloadTypingError(WebPayloadError): pass
 
 
 class WebPayloadMeta(AttributeMeta, TreeMeta, ABCMeta):
     def __init__(cls, *args, **kwargs):
         super(WebPayloadMeta, cls).__init__(*args, **kwargs)
-        cls.__mapping__ = getattr(cls, "__mapping__", {}) | kwargs.get("mapping", {})
         cls.__optional__ = kwargs.get("optional", getattr(cls, "__optional__", False))
-        cls.__multiple__ = kwargs.get("multiple", getattr(cls, "__multiple__", False))
         cls.__locator__ = kwargs.get("locator", getattr(cls, "__locator__", None))
-
-    def __call__(cls, sources, *args, **kwargs):
-        if cls.multiple:
-            if not isinstance(sources, list): raise WebPayloadSingleError()
-            if not bool(sources) and not cls.optional: raise WebPayloadMissingError()
-            instances = []
-            for source in sources:
-                children = {dependent.locator: dependent(source, *args, **kwargs) for dependent in cls.dependents}
-                attributes = dict(children=children) | cls.mapping
-                instance = super(WebPayloadMeta, cls).__call__(source, *args, **attributes, **kwargs)
-                instances.append(instance)
-            return instances
-        else:
-            if isinstance(sources, list): raise WebPayloadMultipleError()
-            if not bool(sources) and not cls.optional: raise WebPayloadMissingError()
-            source = sources
-            children = {dependent.locator: dependent(source, *args, **kwargs) for dependent in cls.dependents}
-            attributes = dict(children=children)
-            instance = super(WebPayloadMeta, cls).__call__(source, *args, **attributes, **kwargs)
-            return instance
 
     @property
     def optional(cls): return cls.__optional__
     @property
+    def locator(cls): return cls.__locator__
+
+
+class WebCollectionMeta(WebPayloadMeta):
+    def __init__(cls, *args, **kwargs):
+        super(WebCollectionMeta, cls).__init__(*args, **kwargs)
+        cls.__multiple__ = kwargs.get("multiple", getattr(cls, "__multiple__", False))
+
+    def __call__(cls, sources, *args, **kwargs):
+        if cls.multiple and not isinstance(sources, list): raise WebPayloadMultipleError()
+        elif not cls.multiple and isinstance(sources, list): raise WebPayloadSingleError()
+        if not cls.optional and not bool(sources): raise WebPayloadMissingError()
+        initialize = lambda source: super(WebCollectionMeta, cls).__call__(source, *args, **kwargs)
+        if cls.multiple: return [initialize(source) for source in sources]
+        else: return initialize(sources)
+
+    @property
     def multiple(cls): return cls.__multiple__
+
+
+class WebMappingMeta(WebPayloadMeta):
+    def __init__(cls, *args, **kwargs):
+        super(WebMappingMeta, cls).__init__(*args, **kwargs)
+        cls.__mapping__ = getattr(cls, "__mapping__", {}) | kwargs.get("mapping", {})
+
+    def __call__(cls, source, *args, **kwargs):
+        children = {dependent.locator: dependent(source, *args, **kwargs) for dependent in cls.dependents}
+        source = children | cls.mapping
+        instance = super(WebMappingMeta, cls).__call__(source)
+        return instance
+
     @property
     def mapping(cls): return cls.__mapping__
-    @property
-    def locator(cls): return cls.__locator__
+
+
+class WebPayloadMappingMeta(WebCollectionMeta, WebMappingMeta): pass
+class WebPayloadTextMeta(WebCollectionMeta): pass
 
 
 class WebPayload(ABC, metaclass=WebPayloadMeta):
     pass
 
 
-class WebPayloadMapping(WebPayload, attribute="Mapping"):
-    pass
+class WebPayloadMapping(WebPayload, metaclass=WebPayloadMappingMeta, attribute="Mapping"): pass
+class WebPayloadText(WebPayload, metaclass=WebPayloadTextMeta, attribute="Text"): pass
 
 
-class WebPayloadValue(WebPayload, attribute="Value"):
-    pass
 
 
 
