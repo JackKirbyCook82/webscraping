63ce45c26cbaabf3970cf72d77780060f860ba3b
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-06-02 21:28:10 -0500

    Updated 1/10/2024

--- webpayloads.py
+++ webpayloads.py
@@ -27,78 +27,55 @@ class WebPayloadTypingError(WebPayloadError): pass
 class WebPayloadMeta(AttributeMeta, TreeMeta, ABCMeta):
     def __init__(cls, *args, **kwargs):
         super(WebPayloadMeta, cls).__init__(*args, **kwargs)
+        cls.__multiple__ = kwargs.get("multiple", getattr(cls, "__multiple__", False))
         cls.__optional__ = kwargs.get("optional", getattr(cls, "__optional__", False))
         cls.__locator__ = kwargs.get("locator", getattr(cls, "__locator__", None))
 
-    def extract(cls, source):
-        if cls.key is not None: return source
-        if not isinstance(source, dict): raise WebPayloadTypingError()
-        if cls.optional and cls.key not in source: raise WebPayloadMissingError()
-        return source[cls.key]
+    def __call__(cls, sources, *args, **kwargs):
+        default = [] if cls.multiple else None
+        sources = sources.get(cls.key, default) if cls.key is not None else sources
+        initialize = lambda source: super(WebPayloadMeta, cls).__call__(source, *args, **kwargs)
+        if cls.multiple:
+            if not isinstance(sources, list): raise WebPayloadSingleError()
+            if not cls.optional and not sources: raise WebPayloadMissingError()
+            instance = [initialize(source) for source in sources]
+        else:
+            if isinstance(sources, list): raise WebPayloadMultipleError()
+            if not cls.optional and sources is None: raise WebPayloadMissingError()
+            instance = initialize(sources)
+        return instance
 
+    @property
+    def multiple(cls): return cls.__multiple__
     @property
     def optional(cls): return cls.__optional__
     @property
     def locator(cls): return cls.__locator__
 
 
-class WebCollectionMeta(WebPayloadMeta):
-    def __init__(cls, *args, **kwargs):
-        super(WebCollectionMeta, cls).__init__(*args, **kwargs)
-        cls.__multiple__ = kwargs.get("multiple", getattr(cls, "__multiple__", False))
-
-    def __call__(cls, sources, *args, **kwargs):
-        if cls.multiple and not isinstance(sources, list): raise WebPayloadSingleError()
-        elif not cls.multiple and isinstance(sources, list): raise WebPayloadMultipleError()
-        if cls.multiple and not cls.optional and not bool(sources): raise WebPayloadMissingError()
-        initialize = lambda source: super(WebCollectionMeta, cls).__call__(source, *args, **kwargs)
-        if cls.multiple: return [initialize(source) for source in sources]
-        else: return initialize(sources)
-
-    @property
-    def multiple(cls): return cls.__multiple__
+class WebPayload(ABC, metaclass=WebPayloadMeta):
+    pass
 
 
-class WebMappingMeta(WebPayloadMeta):
-    def __init__(cls, *args, **kwargs):
-        super(WebMappingMeta, cls).__init__(*args, **kwargs)
-        cls.__static__ = getattr(cls, "__static__", {}) | kwargs.get("static", {})
+class WebPayloadMapping(WebPayload, attribute="Mapping"):
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.static = getattr(cls, "static", {}) | kwargs.get("static", {})
 
-    def __call__(cls, source, *args, **kwargs):
-        source = cls.extract(source)
+    def __new__(cls, source, *args, **kwargs):
         if not isinstance(source, dict): raise WebPayloadTypingError()
-        if not cls.optional and not bool(source): raise WebPayloadMissingError()
-        children = {dependent.locator: dependent(source, *args, **kwargs) for dependent in cls.dependents.values()}
-        return cls.static | children
-
-    @property
-    def static(cls): return cls.__static__
+        dependents = cls.dependents.values()
+        dynamic = {dependent.locator: dependent(source) for dependent in dependents}
+        return cls.static | dynamic
 
 
-class WebTextMeta(WebPayloadMeta):
-    def __init__(cls, *args, **kwargs):
-        super(WebTextMeta, cls).__init__(*args, **kwargs)
-        cls.__parser__ = kwargs.get("parser", getattr(cls, "__parser__", str))
+class WebPayloadText(WebPayload, attribute="Text"):
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.parser = kwargs.get("parser", getattr(cls, "parser", str))
 
-    def __call__(cls, source, *args, **kwargs):
-        source = cls.extract(source)
-        if not isinstance(source, (str, int, float)): raise WebPayloadTypingError()
-        if not cls.optional and source is None: raise WebPayloadMissingError()
+    def __new__(cls, source, *args, **kwargs):
         return cls.parser(source)
-
-    @property
-    def parser(cls): return cls.__parser__
-
-
-class WebPayloadMappingMeta(WebCollectionMeta, WebMappingMeta): pass
-class WebPayloadTextMeta(WebCollectionMeta, WebTextMeta): pass
-
-
-class WebPayload(ABC, metaclass=WebPayloadMeta): pass
-class WebPayloadMapping(WebPayload, metaclass=WebPayloadMappingMeta, attribute="Mapping"): pass
-class WebPayloadText(WebPayload, metaclass=WebPayloadTextMeta, attribute="Text"): pass
-
-
 
 
 
