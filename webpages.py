bc8594a6f055eab716c740cebda1c658d456fe98
JackKirbyCook82 <jackcookchee@yahoo.com>
2026-07-06 03:35:37 -0500

    Updated 1/10/2024

--- webpages.py
+++ webpages.py
@@ -24,9 +24,9 @@ class WebStream(Mixin, ABC):
         cls.__Pages__ = getattr(cls, "__Pages__", {}) | kwargs.get("pages", {})
         cls.__Page__ = kwargs.get("page", getattr(cls, "__Page__", None))
 
-    def __init__(self, *args, source, account=None, authenticator=None, capacity=100, safemode=False, **kwargs):
+    def __init__(self, *args, source, account=None, authenticator=None, capacity=100, **kwargs):
         super().__init__(*args, **kwargs)
-        parameters = dict(source=source, account=account, authenticator=authenticator, safemode=safemode)
+        parameters = dict(source=source, account=account, authenticator=authenticator)
         self.__pages = {key: value(**parameters) for key, value in self.Pages.items()}
         self.__page = self.Page(**parameters) if self.Page is not None else None
         self.__capacity = int(capacity)
@@ -45,11 +45,10 @@ class WebStream(Mixin, ABC):
 
 
 class WebPage(Logging, ABC):
-    def __init__(self, *args, source, account, authenticator, safemode, **kwargs):
+    def __init__(self, *args, source, account, authenticator, **kwargs):
         super().__init__(*args, **kwargs)
         self.__authenticator = authenticator
         self.__account = account
-        self.__safemode = safemode
         self.__source = source
 
     @staticmethod
@@ -62,8 +61,6 @@ class WebPage(Logging, ABC):
     def authenticator(self): return self.__authenticator
     @property
     def account(self): return self.__account
-    @property
-    def safemode(self): return self.__safemode
     @property
     def source(self): return self.__source
 
